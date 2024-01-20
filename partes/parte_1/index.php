6792a99474c6a688ca48172d07c63fb7c5b14a74
Caua <cauamahl@yahoo.com.br>
2024-01-20 10:22:02 -0300

    quebrando a cabeça na p1

--- partes/parte_1/index.php
+++ partes/parte_1/index.php
@@ -22,16 +22,24 @@ $l1 = count($matrizq1); // linhas matriz 1
 $l2 = count($matrizq2); // linhas matriz 2
 $c1 = count($matrizq1[0]); // colunas matriz 1
 $c2 = count($matrizq2[0]); // colunas matriz 2
+$matriznova = []; // nova matriz vazia
 // tenho q multiplicar cada linha  do 1 por cada coluna 2 para obter [m11, m12,][m21, m22]
 for ($l = 0; $l < $l1; $l++) {
-    for ($c = 0; $c < $c2; $c++) {
-        $matriznova[$l][$c] = 0; //criar novo espaço em uma nova matriz
-    };
+//usei array_fill(int $start_index, int $count, mixed $value): array
+    $matriznova[$l] = array_fill(0, $c2, 0);
 };
-var_dump($matriznova);
-for ($l = 0; $l < $l1; $l++) {
-    for ($c = 0; $c < $c2; $c++) {
-        $matrinova[$l][$c] = ($matrizq1[$l][$c]) * ($matrizq2[$l][$c]);
+for ($l = 0; $l < $l1; $l++) { //para cada linha da matriz 1
+    for ($c = 0; $c < $c2; $c++) { //para cada coluna da matriz 2
+        for ($m=0; $m < $c1;$m++){ //para cada linha da coluna 1
+            $matriznova[$l][$c] += $matrizq1[$l][$c] * $matrizq2[$c][$l] ;
+
+        }
     };
-    var_dump($matriznova);
 }
+for ($l = 0; $l < $l1; $l++) { //para cada linha  da matriz 1
+    for ($c = 0; $c < $c2; $c++) { //para cada coluna da matriz 2
+            echo ($matriznova[$l][$c] . " ");
+        }
+        echo "\n";
+    };
+?>
